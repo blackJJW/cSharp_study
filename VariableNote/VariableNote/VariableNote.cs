e8c41c20415bb9e48fc541abbc1e8d2ed1128b56
blackJJW <jjinwoo92@gmail.com>
2024-03-24 01:24:26 +0900

    variable 2

--- VariableNote/VariableNote/VariableNote.cs
+++ VariableNote/VariableNote/VariableNote.cs
@@ -8,5 +8,21 @@ class VariableNote
         i = 1234;
 
         Console.WriteLine(i);
+
+        int number = 7;
+        Console.WriteLine("{0}", number);
+
+        int number1, number2, number3;
+
+        number1 = 1;
+        number2 = 2;
+        number3 = 3;
+
+        Console.WriteLine("{0}, {1}, {2}", number1, number2, number3);
+
+        int a, b, c;
+        a = b = c = 100;
+
+        Console.WriteLine("{0}, {1}, {2}", a, b, c);
     }
 }
