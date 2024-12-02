18be8502b9da37213e106b152fe6e62dab0b0e62
blackJJW <jjinwoo92@gmail.com>
2024-12-02 16:12:04 +0900

    lottery

---
+++ lottery/RandomClassDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+class RandomClassDemo {
+    static void Main() {
+        Console.Write("이번 주의 로또 : ");
+        
+        Random ran = new Random();
+        int[] arr = new int[6];
+        int temp = 0;
+
+        for (int i = 0; i < 6; i++) {
+            temp = ran.Next(45) + 1;
+            bool flag = false;
+
+            if (i > 0 && i < 6) {
+                for (int j = 0; j <= i; j++) {
+                    if (arr[j] == temp) {
+                        flag = true;
+                    }
+                }
+            }
+            if (flag) {
+                --i;
+            } else {
+                arr[i] = temp;
+            }
+        }
+        for (int i = 0; i < 6; i++) {
+            Console.Write("{0} ", arr[i]);
+        }
+        Console.WriteLine();
+    }
+}
