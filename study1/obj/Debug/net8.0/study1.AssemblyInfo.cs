cbf2442ca2e4e3f66f2fb83cc6d5ee0b187dcaf6
blackJJW <jjinwoo92@gmail.com>
2024-12-02 11:13:06 +0900

    add extra

--- study1/obj/Debug/net8.0/study1.AssemblyInfo.cs
+++ study1/obj/Debug/net8.0/study1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("study1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed24164ada6a8312fbf87b35a22fa6407c8e574f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+182ae1ca211349970894ea81e49bb2837d3c2c8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("study1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("study1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
