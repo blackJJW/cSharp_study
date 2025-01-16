0de9d69e453eac72affd868e0ac35dd806d8e160
blackJJW <jjinwoo92@gmail.com>
2025-01-16 10:59:56 +0900

    object instance

---
+++ ObjectNote/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Object {
+    public class Counter {
+        public void GetTodayVisitCount() {
+            Console.WriteLine("오늘 1234명이 접속했습니다.");
+        }
+    }
+    class ObjectNote {
+        static void Main() {
+            Counter counter = new Counter();
+            counter.GetTodayVisitCount();
+        }
+    }
+}
