4c244f495c8593cb99fab974646e10f1080e0456
blackJJW <jjinwoo92@gmail.com>
2025-01-16 10:00:06 +0900

    count Algo

---
+++ CountAlgorithm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class CountAlgorithm {
+    static void Main() {
+        var numbers = Enumerable.Range(1, 1_000).ToArray();
+        int count = default;
+
+        for (int i = 0; i < numbers.Length; i++) {
+            if (numbers[i] % 13 == 0) {
+                count++;
+            }
+        }
+        Console.WriteLine($"1부터 1000까지 정수 중 13의 배수 개수 : {count}");
+    }
+}
